25be99b17fd3b490037cf4f99c4e641a70c0160a
Jiang Yin <jiangyin@gameframework.cn>
2017-09-19 18:54:21 +0800

    更新打包脚本

---
+++ Assets/GameMain/Scripts/Editor/LuaScriptLabelAttacher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace StarForce.Editor
+{
+    public static class LuaScriptLabelAttacher
+    {
+        private const string LuaScriptExtension = ".lua";
+        public const string LuaScriptLabel = "LuaScript";
+
+        [MenuItem("Star Force/Attach LuaScript Label")]
+        public static void AttachLuaScriptLabel()
+        {
+            string[] luaScriptLabels = new string[] { LuaScriptLabel };
+            var luaScriptAssetNames = AssetDatabase.GetAllAssetPaths().Where(luaScriptAssetName => luaScriptAssetName.EndsWith(LuaScriptExtension));
+            foreach (string luaScriptAssetName in luaScriptAssetNames)
+            {
+                Object asset = AssetDatabase.LoadAssetAtPath(luaScriptAssetName, typeof(Object));
+                AssetDatabase.SetLabels(asset, new HashSet<string>(AssetDatabase.GetLabels(asset)).Union(luaScriptLabels).ToArray());
+            }
+
+            Debug.Log("Attach LuaScript label complete.");
+        }
+    }
+}
--- Assets/GameMain/Scripts/Editor/StarForceBuildEventHandler.cs
+++ Assets/GameMain/Scripts/Editor/StarForceBuildEventHandler.cs
@@ -1,5 +1,7 @@
 ﻿using GameFramework;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityGameFramework.Editor.AssetBundleTools;
@@ -8,9 +10,34 @@ namespace StarForce.Editor
 {
     public sealed class StarForceBuildEventHandler : IBuildEventHandler
     {
+        private static readonly IDictionary<string, string> s_LuaScriptNames = new Dictionary<string, string>();
+
         public void PreProcessBuildAll(string productName, string companyName, string gameIdentifier,
             string applicableGameVersion, int internalResourceVersion, string unityVersion, BuildAssetBundleOptions buildOptions, bool zip,
             string outputDirectory, string workingPath, string outputPackagePath, string outputFullPath, string outputPackedPath, string buildReportPath)
+        {
+            CleanStreamingAssets();
+            RenameLuaScripts();
+        }
+
+        public void PostProcessBuildAll(string productName, string companyName, string gameIdentifier,
+            string applicableGameVersion, int internalResourceVersion, string unityVersion, BuildAssetBundleOptions buildOptions, bool zip,
+            string outputDirectory, string workingPath, string outputPackagePath, string outputFullPath, string outputPackedPath, string buildReportPath)
+        {
+            RevertRenameLuaScripts();
+        }
+
+        public void PreProcessBuild(BuildTarget buildTarget, string workingPath, string outputPackagePath, string outputFullPath, string outputPackedPath)
+        {
+
+        }
+
+        public void PostProcessBuild(BuildTarget buildTarget, string workingPath, string outputPackagePath, string outputFullPath, string outputPackedPath)
+        {
+            CopyToStreamingAssets(buildTarget, outputPackagePath);
+        }
+
+        private static void CleanStreamingAssets()
         {
             string streamingAssetsPath = Utility.Path.GetCombinePath(Application.dataPath, "StreamingAssets");
             string[] fileNames = Directory.GetFiles(streamingAssetsPath, "*", SearchOption.AllDirectories);
@@ -27,19 +54,7 @@ namespace StarForce.Editor
             Utility.Path.RemoveEmptyDirectory(streamingAssetsPath);
         }
 
-        public void PostProcessBuildAll(string productName, string companyName, string gameIdentifier,
-            string applicableGameVersion, int internalResourceVersion, string unityVersion, BuildAssetBundleOptions buildOptions, bool zip,
-            string outputDirectory, string workingPath, string outputPackagePath, string outputFullPath, string outputPackedPath, string buildReportPath)
-        {
-
-        }
-
-        public void PreProcessBuild(BuildTarget buildTarget, string workingPath, string outputPackagePath, string outputFullPath, string outputPackedPath)
-        {
-
-        }
-
-        public void PostProcessBuild(BuildTarget buildTarget, string workingPath, string outputPackagePath, string outputFullPath, string outputPackedPath)
+        private static void CopyToStreamingAssets(BuildTarget buildTarget, string assetPath)
         {
             if (buildTarget != BuildTarget.StandaloneWindows)
             {
@@ -47,10 +62,10 @@ namespace StarForce.Editor
             }
 
             string streamingAssetsPath = Utility.Path.GetCombinePath(Application.dataPath, "StreamingAssets");
-            string[] fileNames = Directory.GetFiles(outputPackagePath, "*", SearchOption.AllDirectories);
+            string[] fileNames = Directory.GetFiles(assetPath, "*", SearchOption.AllDirectories);
             foreach (string fileName in fileNames)
             {
-                string destFileName = Utility.Path.GetCombinePath(streamingAssetsPath, fileName.Substring(outputPackagePath.Length));
+                string destFileName = Utility.Path.GetCombinePath(streamingAssetsPath, fileName.Substring(assetPath.Length));
                 FileInfo destFileInfo = new FileInfo(destFileName);
                 if (!destFileInfo.Directory.Exists)
                 {
@@ -59,6 +74,38 @@ namespace StarForce.Editor
 
                 File.Copy(fileName, destFileName);
             }
+        }
+
+        private void RenameLuaScripts()
+        {
+            s_LuaScriptNames.Clear();
+
+            var luaScriptAssetNames = AssetDatabase.FindAssets("l:LuaScript").ToList().ConvertAll(guid => AssetDatabase.GUIDToAssetPath(guid));
+            foreach (string luaScriptAssetName in luaScriptAssetNames)
+            {
+                string luaScriptAssetMetaName = luaScriptAssetName + ".meta";
+                string renamedLuaScriptAssetName = luaScriptAssetName + ".bytes";
+                string renamedLuaScriptAssetMetaName = renamedLuaScriptAssetName + ".meta";
+
+                File.Move(luaScriptAssetName, renamedLuaScriptAssetName);
+                s_LuaScriptNames.Add(luaScriptAssetName, renamedLuaScriptAssetName);
+
+                File.Move(luaScriptAssetMetaName, renamedLuaScriptAssetMetaName);
+                s_LuaScriptNames.Add(luaScriptAssetMetaName, renamedLuaScriptAssetMetaName);
+            }
+
+            AssetDatabase.Refresh();
+        }
+
+        private void RevertRenameLuaScripts()
+        {
+            foreach (var luaScriptNames in s_LuaScriptNames)
+            {
+                File.Move(luaScriptNames.Value, luaScriptNames.Key);
+            }
+
+            s_LuaScriptNames.Clear();
+            AssetDatabase.Refresh();
         }
     }
 }
